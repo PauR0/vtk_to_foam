33d3a499a9159993e503c381b5a3f83b3ae353e9
Pau Romero <pau.romero@uv.es>
2024-03-19 11:09:56 +0000

    Added cylinder test module!

---
+++ test_cylinder.py
@@ -0,0 +1,40 @@
+
+import pyvista as pv
+
+from mesh_to_foam import mesh_to_blockmeshdict
+
+
+cyl = pv.CylinderStructured(radius=[1.0, .9, .8, .7], height=5, direction=[0.0,0.0,1.0]).cast_to_unstructured_grid()
+#cyl.plot()
+cyl_boundary = cyl.extract_surface()
+
+boundary = pv.MultiBlock()
+#Let's define some patches! The last extract_surface ensures all the patches are PolyDatas
+ids = cyl_boundary.points[:, 2] > 2.4 #Top lid
+boundary['top lid'] = cyl_boundary.extract_points(ids, adjacent_cells=False, include_cells=True).extract_surface()
+
+ids = cyl_boundary.points[:, 2] < -2.4 #Bottom lid
+boundary['bottom lid'] = cyl_boundary.extract_points(ids, adjacent_cells=False, include_cells=True).extract_surface()
+
+ids = (cyl_boundary.points[:, 2] > -2.4) & (cyl_boundary.points[:, 2] < 2.4) #Wall
+boundary['wall'] = cyl_boundary.extract_points(ids, adjacent_cells=True, include_cells=True).extract_surface()
+
+mesh = pv.MultiBlock()
+mesh['internalMesh'] = cyl
+mesh['boundary'] = boundary
+
+#Let's visualize what we just built!
+p = pv.Plotter(shape=(1, 2))
+p.subplot(0, 0)
+p.add_mesh(mesh['internalMesh'], color='w', show_edges=True)
+p.show_bounds()
+p.subplot(0, 1)
+p.add_mesh(mesh['boundary'], multi_colors=True)
+
+p.add_axes()
+p.link_views()
+p.show()
+
+bmd = mesh_to_blockmeshdict(mesh)
+
+#print(bmd)
